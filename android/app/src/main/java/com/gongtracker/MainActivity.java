5180c366db5174ebb6739bdda615967ad158e882
Devin Zimmer <dz@renttherunway.com>
2016-05-20 01:13:00 -0400

    crashlytics:android

--- android/app/src/main/java/com/gongtracker/MainActivity.java
+++ android/app/src/main/java/com/gongtracker/MainActivity.java
@@ -3,11 +3,21 @@ package com.gongtracker;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.smixx.fabric.FabricPackage;
+import com.crashlytics.android.Crashlytics;
+import android.os.Bundle;
+import io.fabric.sdk.android.Fabric;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends ReactActivity {
+    /* Fabric */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      Fabric.with(this, new Crashlytics());
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -34,6 +44,7 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
+            new FabricPackage(this),
             new MainReactPackage()
         );
     }
